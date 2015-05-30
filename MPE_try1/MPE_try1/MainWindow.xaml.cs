2a94e96deb4a6f0f638f6b471ca44c204e0e18c5
lwierzch <wierzchnicki@gmail.com>
2015-05-30 23:53:25 +0200

    Project config updated and ecoAgent class created

--- MPE_try1/MPE_try1/MainWindow.xaml.cs
+++ MPE_try1/MPE_try1/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,23 @@ namespace MPE_try1
     /// </summary>
     public partial class MainWindow : Window
     {
+ 
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void AppExit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ShowInfo(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is MPE ecoAgent Simulator, version " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() );
+        }
+
     }
+
+
 }
---
+++ MPE_try1/MPE_try1/eAgent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPE_try1
+{
+    class ecoAgent
+    {
+        // ID Agenta
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        // ID strategii
+        private int strategy_id;
+
+        public int Strategy_id
+        {
+            get { return strategy_id; }
+            set { strategy_id = value; }
+        }
+        // reputacja agenta
+        private int reputation;
+
+        public int Reputation
+        {
+            get { return reputation; }
+            set { reputation = value; }
+        }
+        // pozycja na planszy
+        private int[] position;
+
+        public int[] Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+    }
+}
