44ec61f40cf571b2f1837c473df4f9caad611519
lwierzch <wierzchnicki@gmail.com>
2015-06-04 01:05:52 +0200

    working visualization

--- MPE_try1/MPE_try1/MainWindow.xaml.cs
+++ MPE_try1/MPE_try1/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,12 +24,20 @@ namespace MPE_try1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static Dictionary<string, Ellipse> ellipseList;
+        private static Storyboard storTmp;
+        private static int ileAgentow;
 
         public MainWindow()
         {
             InitializeComponent();
-            eVisualizer pokazywacz = new eVisualizer();
+            ileAgentow = 30;
+            eVisualizer pokazywacz = new eVisualizer(ileAgentow);
             Application.Current.Resources.MergedDictionaries.Add(pokazywacz.pathsDict);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+            ellipseList = new Dictionary<string, Ellipse>();
+            storTmp = new Storyboard();
+            
         }
 
         private void AppExit(object sender, RoutedEventArgs e)
@@ -47,25 +57,65 @@ namespace MPE_try1
             //    Ellipse eli+idx.ToString() = new Ellipse();
             //    MainCanvas.Children.Add(eli)
             //}
-            Dictionary<string, Ellipse> ellipseList = new Dictionary<string, Ellipse>();
-            for (int idx = 1; idx <= 20; idx++)
+
+
+            for (int idx = 1; idx <= ileAgentow; idx++)
             {
                 Ellipse eliTmp = new Ellipse();
+                Path linTmp = new Path();
+                DoubleAnimationUsingPath aniXTmp = new DoubleAnimationUsingPath();
+                DoubleAnimationUsingPath aniYTmp = new DoubleAnimationUsingPath();
+
+                storTmp.AutoReverse = true;
+                storTmp.RepeatBehavior = System.Windows.Media.Animation.RepeatBehavior.Forever;
+                eliTmp.Name = "ellipse" + idx;
+                linTmp.Name = "linia" + idx;
+                aniXTmp.Name = "animationX" + idx;
+                aniYTmp.Name = "animationY" + idx;
+                //storTmp.Name = "storyboard" + idx;
+
                 eliTmp.Width = 10;
                 eliTmp.Height = 10;
+                eliTmp.Fill = new SolidColorBrush(Colors.Red);
                 eliTmp.RenderTransform = new TranslateTransform();
-                EventTrigger evTr = new EventTrigger();
-                //evTr.RoutedEvent = new RoutedEvent("Path.Loaded", RoutingStrategy.Bubble, typeof(RoutedEventHandler));
-                eliTmp.Triggers.Add(evTr);
+                //EventTrigger evTr = new EventTrigger();
+                //evTr.RoutedEvent = EventManager.RegisterRoutedEvent("Path.Loaded" + idx, RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(Ellipse));
+                //eliTmp.Triggers.Add(evTr);
                 ellipseList.Add("Ellipse" + idx.ToString(), eliTmp);
-                linia.Data = Geometry.Parse((string)TryFindResource("AnimationPath" + idx));
-                RuchX.PathGeometry = PathGeometry.Parse((string)TryFindResource("AnimationPath" + idx)).GetWidenedPathGeometry(new Pen());
-                RuchY.PathGeometry = PathGeometry.Parse((string)TryFindResource("AnimationPath" + idx)).GetWidenedPathGeometry(new Pen());
-                AgentMovement.Storyboard.Begin();
+
+                linTmp.Data = Geometry.Parse((string)TryFindResource("AnimationPath" + idx));
+                linTmp.Stroke = new SolidColorBrush(Colors.Yellow);
+
+                aniXTmp.Name = "RuchX" + idx;
+                aniXTmp.Source = PathAnimationSource.X;
+                aniXTmp.Duration = TimeSpan.FromSeconds(20);
+                Storyboard.SetTarget(aniXTmp, eliTmp);
+                Storyboard.SetTargetProperty(aniXTmp, new PropertyPath(Canvas.LeftProperty));
+                aniYTmp.Name = "RuchY" + idx;
+                aniYTmp.Source = PathAnimationSource.Y;
+                aniYTmp.Duration = TimeSpan.FromSeconds(20);
+                Storyboard.SetTarget(aniYTmp, eliTmp);
+                Storyboard.SetTargetProperty(aniYTmp, new PropertyPath(Canvas.TopProperty));
+
+                aniXTmp.PathGeometry = PathGeometry.Parse((string)TryFindResource("AnimationPath" + idx)).GetWidenedPathGeometry(new Pen());
+                aniYTmp.PathGeometry = PathGeometry.Parse((string)TryFindResource("AnimationPath" + idx)).GetWidenedPathGeometry(new Pen());
+
+                MainCanvas.Children.Add(eliTmp);
+                MainCanvas.Children.Add(linTmp);
+                storTmp.Children.Add(aniXTmp);
+                storTmp.Children.Add(aniYTmp);
+
             }
+            storTmp.Begin(this.MainCanvas, true);
 
 
+        }
 
+        private void PauseV(object sender, RoutedEventArgs e)
+        {
+            if (storTmp.GetCurrentState() == System.Windows.Media.Animation.ClockState.Active)
+            { storTmp.Pause(this.MainCanvas); }
+            else { storTmp.Resume(this.MainCanvas); }
         }
 
     }
--- MPE_try1/MPE_try1/eVisualizer.cs
+++ MPE_try1/MPE_try1/eVisualizer.cs
@@ -10,17 +10,17 @@ namespace MPE_try1
     public class eVisualizer
     {
         public ResourceDictionary pathsDict;
-        public eVisualizer()
+        public eVisualizer(int upperLimit)
         {
             pathsDict = new ResourceDictionary();
             Random randGen = new Random();
             int[] tabXY = new int[14];
 
-            for (int idx = 1; idx <= 20; idx++)
+            for (int idx = 1; idx <= upperLimit; idx++)
             {
                 for (int position = 0; position < 14; position++)
                 {
-                    tabXY[position] = randGen.Next(500);
+                    tabXY[position] = randGen.Next(690);
                 }
                 pathsDict.Add("AnimationPath" + idx.ToString(), "M " + tabXY[0].ToString() + "," + tabXY[1].ToString() + " C " + tabXY[2].ToString() + "," + tabXY[3].ToString() + " "
                     + tabXY[4].ToString() + "," + tabXY[5].ToString() + " " + tabXY[6].ToString() + "," + tabXY[7].ToString() + " " + tabXY[8].ToString() + "," + tabXY[9].ToString()
